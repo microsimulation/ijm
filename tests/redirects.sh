f0e43024b2058a2c0af15095168cb3870d372ed8
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-22 10:13:02 +0000

    doi that actually has spaces in target url #108

--- tests/redirects.sh
+++ tests/redirects.sh
@@ -5,7 +5,7 @@ dois=(
     10.34196/ijm.00205  # issue 12_3
     10.34196/ijm.00198  # issue 12_2
     10.34196/ijm.00193  # issue 12_1 (map rule)
-    10.34196/ijm.00057  # issue 4_3 (map rule with spaces)
+    10.34196/ijm.00056  # issue 4_3 (map rule with spaces)
     10.34196/ijm.00003  # issue 1_1
 )
 
