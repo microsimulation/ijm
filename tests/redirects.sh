74bed5a0d7a23bb2b8596ae94ec720204c296b55
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-12-22 15:15:32 +0000

    add broken doi to redirect.sh test #110

--- tests/redirects.sh
+++ tests/redirects.sh
@@ -7,6 +7,11 @@ dois=(
     10.34196/ijm.00193  # issue 12_1 (map rule)
     10.34196/ijm.00056  # issue 4_3 (map rule with spaces)
     10.34196/ijm.00003  # issue 1_1
+    10.34196/ijm.00045  # found via all_doi.sh
+    10.34196/ijm.00081  # found via all_doi.sh
+    10.34196/ijm.00104  # found via all_doi.sh
+    10.34196/ijm.00084  # found via all_doi.sh
+    10.34196/ijm.00083  # found via all_doi.sh
 )
 
 for doi in "${dois[@]}"; do
